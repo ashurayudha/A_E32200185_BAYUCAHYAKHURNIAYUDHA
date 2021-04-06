54ffe59e67dd4137c958e2a463b89b50a0a30522
ashura yudha <ashurayudha@gmail.com>
2021-04-07 02:05:03 +0700

    Minggu 1-5
    E32200185

---
+++ Minggu ke1-5/BayuBelajarLaravel/resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getlocale())}}">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-widh, initial-scale1.0">
+	<meta http-equiv="X-AU-Compitable">
+	<title>{{ config('app.name')}}</title>
+</head>
+<body>
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+			@yield('content')
+	</div>
+ </div>
+</body>
+</html>
---
+++ Minggu ke1-5/BayuBelajarLaravel/resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+	<div class="jumbotron jumbotron-fluide">
+		<div class="container">
+			<h1 class="display-4">Home Page</h1>
+			<p class="lead">this is the Home Page</p>
+		</div>
+	</div>
+@endsection
