509088eb664c0daadf6613c71f136e9c65c86de7
ashura yudha <ashurayudha@gmail.com>
2021-04-23 21:16:03 +0700

    Minggu 7

---
+++ Minggu ke 7/BayuBelajarLaravel/resources/views/gambar.blade.php
@@ -0,0 +1,4 @@
+<img src= "{{ asset('img/gambar 1.jpg') }}" alt="">
+<img src= "{{ asset('img/gambar 2.jpg') }}" alt="" >
+<img src= "{{ asset('img/gambar 3.jpg') }}" alt="">
+<img src= "{{ asset('img/gambar 4.jpg') }}" alt="">
